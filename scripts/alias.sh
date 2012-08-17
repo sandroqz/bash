59e19f2e7f01f2d79a2d43ee18c9e4ee69e2510f
Sandro Queiroz Jr <sandroqz@gmail.com>
2012-08-17 17:45:57 -0300

    Novos aliases

--- scripts/alias.sh
+++ scripts/alias.sh
@@ -2,7 +2,10 @@ alias ll="ls -Glah"
 ###alias ll="ls -Glahs"
 
 alias ls="/bin/ls -G"
-alias top="top -o cpu"
+
+# update every 5 seconds
+alias topc="top -o cpu -s 5"
+alias topm="top -o mreg -s 5"
 
 alias cls='clear'
 alias sizes='du -h -d1'
