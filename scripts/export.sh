a1c73a3af0637c45db01f1f87490dce9f903ad3c
Sandro Queiroz Jr <sandroqz@gmail.com>
2012-08-25 17:23:31 -0300

    added a blank line for better organization

--- scripts/export.sh
+++ scripts/export.sh
@@ -1,5 +1,6 @@
 ## PATH
 # Put /usr/local/{sbin,bin} first
 export PATH=/usr/local/bin:/usr/local/sbin:$PATH
+
 export PATH=$PATH:$HOME/.rvm/bin # Add RVM to PATH for scripting
 export JAVA_HOME="/System/Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Home"
