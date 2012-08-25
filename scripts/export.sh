58c12561d244a1602b95f8f03d96e56bd1214934
Sandro Queiroz Jr <sandroqz@gmail.com>
2012-08-25 16:37:23 -0300

    using export command to add rvm to path

--- scripts/export.sh
+++ scripts/export.sh
@@ -2,4 +2,4 @@
 # Put /usr/local/{sbin,bin} first
 export PATH=/usr/local/bin:/usr/local/sbin:$PATH
 
-PATH=$PATH:$HOME/.rvm/bin # Add RVM to PATH for scripting
+export PATH=$PATH:$HOME/.rvm/bin # Add RVM to PATH for scripting
