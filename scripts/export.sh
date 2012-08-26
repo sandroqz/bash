127345c0fbc23cc9e5e81eb6b6ae0def78ae481b
Sandro Queiroz Jr <sandroqz@gmail.com>
2012-08-26 15:08:28 -0300

    some updates in export.sh

--- scripts/export.sh
+++ scripts/export.sh
@@ -1,6 +1,2 @@
-## PATH
-# Put /usr/local/{sbin,bin} first
-export PATH=/usr/local/bin:/usr/local/sbin:$PATH
-
-export PATH=$PATH:$HOME/.rvm/bin # Add RVM to PATH for scripting
+export PATH="/usr/local/bin:/usr/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/X11/bin"
 export JAVA_HOME="/System/Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Home"
