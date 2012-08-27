8925fa99f5e097569954611aa8a0f6958af2904c
Sandro Queiroz Jr <sandroqz@gmail.com>
2012-08-27 03:35:37 -0300

    spring roo environment variables added

--- scripts/export.sh
+++ scripts/export.sh
@@ -4,5 +4,8 @@ export M2_HOME="$HOME/Development/apache-maven/apache-maven-3.0.4"
 export M2="$M2_HOME/bin"
 export PATH="$M2:$PATH"
 
+export ROO_HOME="$HOME/Development/spring-roo-1.2.2.RELEASE"
+#export ROO_OPTS="-Droo.bright=true"
+
 export JAVA_HOME="/System/Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Home"
 export PATH="$JAVA_HOME/bin:$PATH"
