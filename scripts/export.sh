6f0d49c68b609e0130f5d418ff89e526a5300f73
Sandro Queiroz Jr <sandroqz@gmail.com>
2012-08-27 01:19:07 -0300

    added environment variables for Apache Maven

--- scripts/export.sh
+++ scripts/export.sh
@@ -1,2 +1,8 @@
 export PATH="/usr/local/bin:/usr/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/X11/bin"
+
+export M2_HOME="$HOME/Development/apache-maven/apache-maven-3.0.4"
+export M2="$M2_HOME/bin"
+export PATH="$M2:$PATH"
+
 export JAVA_HOME="/System/Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Home"
+export PATH="$JAVA_HOME/bin:$PATH"
