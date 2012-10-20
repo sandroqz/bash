53d6b2137d2ecf3d2bf7bc735d135546a88b048b
Sandro Queiroz Jr <sandroqz@gmail.com>
2012-10-20 12:18:25 -0300

    Now using Java JDK7

--- scripts/export.sh
+++ scripts/export.sh
@@ -7,5 +7,5 @@ export PATH="$M2:$PATH"
 export ROO_HOME="$HOME/Development/spring-roo-1.2.2.RELEASE"
 #export ROO_OPTS="-Droo.bright=true"
 
-export JAVA_HOME="/System/Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Home"
+export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk1.7.0_09.jdk/Contents/Home"
 export PATH="$JAVA_HOME/bin:$PATH"
