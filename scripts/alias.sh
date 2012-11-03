be8c2172ad5d9d75a97bf99a6207b0bca2f95630
Sandro Queiroz Jr <sandroqz@gmail.com>
2012-11-03 05:22:45 -0300

    some modifications

--- scripts/alias.sh
+++ scripts/alias.sh
@@ -3,10 +3,11 @@ alias ll="ls -Glah"
 
 alias ls="/bin/ls -G"
 alias mysql="mysql --auto-vertical-output"
+alias sql3="sqlite3 -line"
 
-# update every 5 seconds
-alias topc="top -o cpu -s 5"
-alias topm="top -o mreg -s 5"
+# update every 3 seconds
+alias topc="top -o cpu -s 3"
+alias topm="top -o mreg -s 3"
 
 alias cls='clear'
 alias sizes='du -h -d1'
--- scripts/export.sh
+++ scripts/export.sh
@@ -1,14 +1,16 @@
 export PATH="/usr/local/bin:/usr/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/X11/bin"
 
 export GREP_OPTIONS="--color=auto"
-export GREP_COLOR="4;31"
+export GREP_COLOR="1;31"
 
 export M2_HOME="$HOME/Development/apache-maven/apache-maven-3.0.4"
 export M2="$M2_HOME/bin"
 export PATH="$M2:$PATH"
+export MAVEN_OPTS="-Xms256m -Xmx1024m -XX:MaxPermSize=512m"
 
 export ROO_HOME="$HOME/Development/spring-roo-1.2.2.RELEASE"
 #export ROO_OPTS="-Droo.bright=true"
 
-export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk1.7.0_09.jdk/Contents/Home"
+###export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk1.7.0_09.jdk/Contents/Home"
+export JAVA_HOME="/System/Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Home"
 export PATH="$JAVA_HOME/bin:$PATH"
