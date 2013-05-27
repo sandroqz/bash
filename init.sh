03966168442ddacadd8ef85dc02ff5c91183d28c
Sandro Queiroz Jr <sandroqz@gmail.com>
2013-05-27 03:11:06 -0300

    Now using another git-completion

--- init.sh
+++ init.sh
@@ -4,7 +4,8 @@ source $HOME/.bash/scripts/export.sh
 source $HOME/.bash/scripts/alias.sh
 #source $HOME/.bash/scripts/options.sh
 
-source $HOME/.bash/scripts/git_completion.sh
+source /usr/local/etc/bash_completion.d/git-completion.bash
+source /usr/local/etc/bash_completion.d/git-prompt.sh
 source $HOME/.bash/scripts/gem_completion.sh
 
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
