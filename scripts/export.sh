1b4fbc26ba150ce4b3f8953a6ac67aebc5f612a9
Sandro Queiroz Jr <sandroqz@gmail.com>
2013-08-20 03:18:23 -0300

    Not using Spring Roo anymore

--- scripts/export.sh
+++ scripts/export.sh
@@ -14,8 +14,7 @@ export M2="$M2_HOME/bin"
 export PATH="$M2:$PATH"
 export MAVEN_OPTS="-Xms256m -Xmx1024m -XX:MaxPermSize=512m"
 
-export ROO_HOME="$HOME/Development/spring-roo-1.2.2.RELEASE"
-#export ROO_OPTS="-Droo.bright=true"
+###export ROO_OPTS="-Droo.bright=true"
 
 export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk1.7.0_09.jdk/Contents/Home"
 ###export JAVA_HOME="/System/Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Home"
