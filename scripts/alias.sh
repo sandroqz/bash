172e9473f41682a20d00ae883e8edfaf1314aebd
Sandro Queiroz Jr <sandroqz@gmail.com>
2016-11-01 21:36:50 -0300

    Some changes to alias.sh
    * Renames an alias from sqlite3 to sqlite and fixes its directory path;
    * Adds two more alias: scrspec and slrspec.

--- scripts/alias.sh
+++ scripts/alias.sh
@@ -3,7 +3,7 @@ alias ll="ls -Glah"
 
 alias ls="/bin/ls -G"
 alias mysql="mysql --auto-vertical-output"
-alias sqlite3="/usr/local/Cellar/sqlite/3.7.17/bin/sqlite3 -line"
+alias sqlite="/usr/local/Cellar/sqlite/3.15.0/bin/sqlite3 -line"
 
 # update every 3 seconds
 alias topc="top -o cpu -s 3"
@@ -17,5 +17,8 @@ alias rvmcheck='type rvm | head -1'
 alias psql="/usr/local/bin/psql"
 
 alias migrateall="rake db:migrate && RAILS_ENV=test rake db:migrate"
+
+alias scrspec="SCREENSHOT=t rspec"
+alias slrspec="SELENIUM=t rspec"
 
 alias tree="tree -C"
