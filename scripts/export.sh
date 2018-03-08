b3f7e51ffe0000c33cb167822e261ae0b5a92ffe
Sandro Queiroz Jr <sandroqz@gmail.com>
2018-03-07 23:32:04 -0300

    Fix PATH

--- scripts/export.sh
+++ scripts/export.sh
@@ -1,4 +1,4 @@
-export PATH="/usr/local/bin:/usr/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/X11/bin"
+export PATH="/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin:/usr/X11/bin"
 
 if [[ "$(uname)" != "Darwin" ]]; then
   export EDITOR=vim
